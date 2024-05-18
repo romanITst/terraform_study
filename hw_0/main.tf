071b136ef68e3530f764c15d311aeeaa2a81d3f6
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-18 13:22:10 +0600

    homework files uploaded

---
+++ hw_0/main.tf
@@ -0,0 +1,27 @@
+resource "random_password" "user_password" {
+    length           = 10
+    special          = true
+    override_special = "(){}[]<>+-=_:?!#&*"
+    min_special      = 3
+    min_numeric      = 2
+}
+
+resource "random_password" "admin_password" {
+    length           = 14
+    special          = true
+    override_special = "(){}[]<>+-=_:?!#&*"
+    min_special      = 4
+    min_numeric      = 4 
+}
+
+resource "local_file" "user_password" {
+    filename = "./user_password.txt"
+    content  = random_password.user_password.result
+    file_permission = 0700
+}
+
+resource "local_file" "admin_password" {
+    filename = "./admin_password.txt"
+    content  = random_password.admin_password.result
+    file_permission = 0700
+}

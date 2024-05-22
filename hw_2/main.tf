98525225be6cbcfdc5cfb4fc0e04a366aea0c974
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-22 20:12:06 +0600

    configuration file uploaded

---
+++ hw_2/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        local = {
+            source = "hashicorp/local"
+            version = "2.1.0"
+        }
+    }
+}
+
+
+resource "local_file" "ip_addresses" {
+    filename = "./ip_addresses.txt"
+    content = join("\n", var.list_ip_addresses)
+
+    
+}
+
+resource "local_file" "surname_name" {
+    filename = "./personal_info.txt"
+    content = var.surname_name
+}
+
+resource "local_file" "unique_ids" {
+    filename = "./unique_ids.txt"
+    content = join("\n", var.unique_ids)
+}

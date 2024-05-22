038373733e924aae5139653a4aa5f414d05fa2cc
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-22 20:44:55 +0600

    file permissions changed

--- hw_2/main.tf
+++ hw_2/main.tf
@@ -11,16 +11,17 @@ terraform {
 resource "local_file" "ip_addresses" {
     filename = "./ip_addresses.txt"
     content = join("\n", var.list_ip_addresses)
-
-    
+    file_permission = 0700
 }
 
 resource "local_file" "surname_name" {
     filename = "./personal_info.txt"
     content = var.surname_name
+    file_permission = 0700
 }
 
 resource "local_file" "unique_ids" {
     filename = "./unique_ids.txt"
     content = join("\n", var.unique_ids)
+    file_permission = 0700
 }

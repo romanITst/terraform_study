20e26d59188e7002384b512521e85d7cb07f95a3
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-22 15:43:41 +0600

    versions of providers have been changed due to homework requirements

--- hw_1/main.tf
+++ hw_1/main.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers {
         local = {
             source = "hashicorp/local"
-            version = "2.5.1"
+            version = "2.1.0"
         }
 
         random = {
             source = "hashicorp/random"
-            version = "3.6.1"
+            version = "3.1.0"
         }
     }
 }

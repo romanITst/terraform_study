5ef43ae73a6c133df0d79c26fc014fe00077628d
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-22 15:27:02 +0600

    required_providers block added

--- hw_1/main.tf
+++ hw_1/main.tf
@@ -1,3 +1,18 @@
+terraform {
+    required_providers {
+        local = {
+            source = "hashicorp/local"
+            version = "2.1.0"
+        }
+
+        random = {
+            source = "hashicorp/random"
+            version = "3.1.0"
+        }
+    }
+}
+
+
 resource "random_password" "user_password" {
     length           = 10
     special          = true

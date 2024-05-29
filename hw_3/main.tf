f3a106da9acac3c67909ae397486e048e9a0bef9
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-29 20:49:05 +0600

    directory permissions added

--- hw_3/main.tf
+++ hw_3/main.tf
@@ -10,18 +10,21 @@ terraform {
 resource "local_file" "list_ip_addresses" {
     filename        = "./ip_addresses.txt"
     content         = join("\n", var.list_ip_addresses)
-    file_permission = 0700
+    file_permission      = 700
+    directory_permission = 700
 }
 resource "local_file" "surname_name" {
     filename        = "./personal_info.txt"
     content         = var.surname_name
     depends_on      = [local_file.list_ip_addresses]
-    file_permission = 0700
+    file_permission      = 700
+    directory_permission = 700
 }
 
 resource "local_file" "unique_ids" {
     filename        = "./unique_ids.txt"
     content         = join("\n", var.unique_ids)
     depends_on      = [local_file.surname_name]
-    file_permission = 0700
+    file_permission      = 700
+    directory_permission = 700
 }

4ad4ad2825f753bdd609a6c9c68f2d76d5a1ba7a
Roman Kurtasov <rkurtasov@gmail.com>
2024-05-29 20:38:03 +0600

    ""/n" on resources changed to "\n". my bad -_-"

--- hw_3/main.tf
+++ hw_3/main.tf
@@ -9,15 +9,17 @@ terraform {
 
 resource "local_file" "list_ip_addresses" {
     filename = "./ip_addresses.txt"
-    content  = join("/n", var.list_ip_addresses)
+    content  = join("\n", var.list_ip_addresses)
 }
 
 resource "local_file" "surname_name" {
-    filename = "./personal_info.txt"
-    content  = var.surname_name
+    filename   = "./personal_info.txt"
+    content    = var.surname_name
+    depends_on = [local_file.list_ip_addresses]
 }
 
 resource "local_file" "unique_ids" {
     filename = "./unique_ids.txt"
-    content  = join("/n", var.unique_ids)
+    content  = join("\n", var.unique_ids)
+    depends_on = [local_file.surname_name]
 }

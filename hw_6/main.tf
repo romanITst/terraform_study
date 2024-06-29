93ecfc219419cb68e8690b521152ad438e77a935
romanITst <rkurtasov@gmail.com>
2024-06-29 17:54:16 +0600

    'locals' block was removed, such as condition in null_resource.file_create

--- hw_6/main.tf
+++ hw_6/main.tf
@@ -1,21 +1,13 @@
-locals {
-    zero_instance_size = 0
-    instance_size = var.instance_size == "small" ? "small" : (var.instance_size == "large" ? "large" : local.zero_instance_size) 
-
-}
 
 resource "null_resource" "file_create" {
     depends_on = [ var.instance_size ]
     provisioner "local-exec" {
-        #command = local.instance_size == "small" ? "touch ./instance_info.txt" : (local.instance_size == "large" ? "touch ./instance_info.txt" : "echo File can't be touched. Check the 'var.instance_size'")
-        command = "touch ./instance_size.txt"
+        command = "touch ./instance_info.txt"
     }
 }
 
 resource "local_file" "file_info" {
-    depends_on  = [
-        null_resource.file_create
-    ]
+    depends_on  = [null_resource.file_create ]
     filename    = "./instance_info.txt"
-    content     = var.instance_size == "small" ? "1 cpu" : (var.instance_size == "large" ? "4 cpu" : "Error! Check the 'var.instance_size'. It must be 'small' or 'large'.")
+    content     = var.instance_size == "small" ? "instance size = 1 cpu" : (var.instance_size == "large" ? "instance_size = 4 cpu" : "var.instance_size's condition will stop execution of this code")
 }

c97028d74ae87280fc7ff7ed452cdad0d044985c
romanITst <rkurtasov@gmail.com>
2024-06-29 17:32:55 +0600

    validation block to 'instance_size' var added

--- hw_6/variables.tf
+++ hw_6/variables.tf
@@ -1,4 +1,8 @@
 variable "instance_size" {
     type = string
     description = "value"
+    validation {
+        condition = substr(var.instance_size, 0, 5) == "large" || substr(var.instance_size, 0, 5) == "small"
+        error_message = "value"
+    }
 }

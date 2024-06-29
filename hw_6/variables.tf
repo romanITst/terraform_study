2a0623a16d0fe600405535a95ed460a10f2f9974
romanITst <rkurtasov@gmail.com>
2024-06-29 17:47:27 +0600

    length value added to instance_size variable condition

--- hw_6/variables.tf
+++ hw_6/variables.tf
@@ -2,7 +2,7 @@ variable "instance_size" {
     type = string
     description = "Count of instance cpu's. It must be small(1cpu) or large(4cpu)"
     validation {
-        condition = substr(var.instance_size, 0, 5) == "large" || substr(var.instance_size, 0, 5) == "small"
+        condition = length(var.instance_size) == 5 && (substr(var.instance_size, 0, 5) == "large" || substr(var.instance_size, 0, 5) == "small")
         error_message = "The value of 'var.instance_size' must be string and 'small' or 'large'"
     }
 }

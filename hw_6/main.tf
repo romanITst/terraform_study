9875cd20b651114747e4f38245455c500480ca8b
romanITst <rkurtasov@gmail.com>
2024-06-30 13:26:44 +0600

    terraform fmt cmd applyed

--- hw_6/main.tf
+++ hw_6/main.tf
@@ -1,13 +1,13 @@
 
 resource "null_resource" "file_create" {
-    depends_on = [ var.instance_size ]
-    provisioner "local-exec" {
-        command = "touch ./instance_info.txt"
-    }
+  depends_on = [var.instance_size]
+  provisioner "local-exec" {
+    command = "touch ./instance_info.txt"
+  }
 }
 
 resource "local_file" "file_info" {
-    depends_on  = [null_resource.file_create ]
-    filename    = "./instance_info.txt"
-    content     = var.instance_size == "small" ? "instance size = 1 cpu" : (var.instance_size == "large" ? "instance_size = 4 cpu" : "var.instance_size's condition will stop execution of this code")
+  depends_on = [null_resource.file_create]
+  filename   = "./instance_info.txt"
+  content    = var.instance_size == "small" ? "instance size = 1 cpu" : (var.instance_size == "large" ? "instance_size = 4 cpu" : "var.instance_size's condition will stop execution of this code")
 }
--- hw_6/variables.tf
+++ hw_6/variables.tf
@@ -1,8 +1,8 @@
 variable "instance_size" {
-    type = string
-    description = "Count of instance cpu's. It must be small(1cpu) or large(4cpu)"
-    validation {
-        condition = length(var.instance_size) == 5 && (substr(var.instance_size, 0, 5) == "large" || substr(var.instance_size, 0, 5) == "small")
-        error_message = "The value of 'var.instance_size' must be string and 'small' or 'large'"
-    }
+  type        = string
+  description = "Count of instance cpu's. It must be small(1cpu) or large(4cpu)"
+  validation {
+    condition     = length(var.instance_size) == 5 && (substr(var.instance_size, 0, 5) == "large" || substr(var.instance_size, 0, 5) == "small")
+    error_message = "The value of 'var.instance_size' must be string and 'small' or 'large'"
+  }
 }

74c1910d3512aafb269d49b8f4e519bb8085df14
romanITst <rkurtasov@gmail.com>
2024-06-30 15:44:37 +0600

    output's description's update

--- hw_8/output.tf
+++ hw_8/output.tf
@@ -1,14 +1,14 @@
 output "time0" {
   value       = local_file.time0.content
-  description = "Contains current date & time"
+  description = "Contains current date & time at the time of execution"
 }
 
 output "time1" {
   value       = local_file.time1.content
-  description = "Contains current date & time"
+  description = "Contains current date & time at the time of execution"
 }
 
 output "tf_version" {
   value       = local_file.tf_version.content
-  description = "Contains current terraform version"
+  description = "Contains current terraform version at the time of execution"
 }

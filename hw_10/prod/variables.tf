75ca53a9ae0072c15eed6155d99464705363fccd
romanITst <rkurtasov@gmail.com>
2024-07-14 14:57:58 +0600

    variables descriptions added

--- hw_10/prod/variables.tf
+++ hw_10/prod/variables.tf
@@ -1,14 +1,14 @@
 variable "filename" {
   type        = string
-  description = "set the file name"
+  description = "The name of .cfg file with web-server configuration for 'prod' environment"
 }
 
 variable "content" {
   type        = string
-  description = "put the content"
+  description = "The content of .cfg file which should consist of server_ip & server_port & server_name'"
 }
 
 variable "file_permission" {
   type        = number
-  description = "set the permission"
+  description = "The .cfg file permission"
 }

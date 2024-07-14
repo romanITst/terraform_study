6aeaa02200fcc8fb4d1dbd3ce8173eb6144dac32
romanITst <rkurtasov@gmail.com>
2024-07-14 15:01:38 +0600

    descriptions and values for outputs added

--- hw_10/prod/output.tf
+++ hw_10/prod/output.tf
@@ -1,11 +1,14 @@
 output "filename" {
-    value = "The name of .cfg file with web-server configuration for 'prod' environment"
+    value = var.filename
+    description = "The name of .cfg file with web-server configuration for 'prod' environment"
 }
 
 output "file_permission" {
-    value = "The .cfg file permission"
+    value = var.file_permission
+    description = "The .cfg file permission"
 }
 
 output "content" {
-    value = "The content of .cfg file which should consist of server_ip & server_port & server_name'"
+    value = var.content
+    description = "The content of .cfg file which should consist of server_ip & server_port & server_name'"
 }

46129517c6b070fbea84e0b62cb278310bf3180a
romanITst <rkurtasov@gmail.com>
2024-07-14 14:56:59 +0600

    initial commit, output's

---
+++ hw_10/prod/output.tf
@@ -0,0 +1,11 @@
+output "filename" {
+    value = "The name of .cfg file with web-server configuration for 'prod' environment"
+}
+
+output "file_permission" {
+    value = "The .cfg file permission"
+}
+
+output "content" {
+    value = "The content of .cfg file which should consist of server_ip & server_port & server_name'"
+}
